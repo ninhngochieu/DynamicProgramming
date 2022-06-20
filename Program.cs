d52be64c2b802277a202d74f29589169e7b397e3
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2022-06-20 22:35:10 +0700

    Complete Dynamic Programming Course

--- Program.cs
+++ Program.cs
@@ -38,6 +38,7 @@ namespace DynamicProgramming
 
         static void Main(string[] args)
         {
+            #region Recursion
             //Fib(40);
             //GridTraversal(20, 20);
             //CanSum(7, new List<int>() { 5,3,4,7});
@@ -56,7 +57,7 @@ namespace DynamicProgramming
             //CountSum(7, new List<int>() { 2, 4 });
             //CountSum(300, new List<int>() { 7, 14 });
             //AllSum(8, new List<int>() { 2, 3, 5 });
-            //AllSum(8, new List<int>() { 3, 5, 2 });
+            AllSum(40, new List<int>() { 2,3,5 });
             //AllSum(7, new List<int>() { 2, 4 });
             //AllSum(300, new List<int>() { 7, 14 });
             //canConstruct("abcdef", new List<string>() { "ab","abc","cd","def","abcd"});
@@ -73,8 +74,376 @@ namespace DynamicProgramming
             //AllConstruct("skateboard", new List<string>() { "bo", "rd", "ate", "t", "ska", "sk", "boar" });
             //AllConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
             //    new List<string>() { "eeeee", "eeeeeee", "eeeeeeee", "eeeeeeeeeee", "eeeeeeeeeeee", "eeeeeeeeeeeeee", "eeeeeeeeeeeeee" });
-            
-            Console.WriteLine("OK");
+
+            #endregion
+            #region Tabulation
+            ///Visualize thử trên dạng bảng
+            ///Size của bảng dựa trên input
+            ///Khởi tạo giá trị bảng với giá trị mặc định
+            ///Seed câu trả lời tầm thường vào bảng
+            ///Duyệt bảng
+            //FibTab(50);
+            //GridTraversalTab(3, 3);
+            //CanSumTab(8, new List<int>() { 2, 3, 5 });
+            //CanSumTab(7, new List<int>() { 5, 3, 4 });
+            //CanSumTab(300, new List<int>() { 7,14});
+            //HowSumTab(8, new List<int>() { 2, 3, 5 });
+            //HowSumTab(7, new List<int>() { 5, 3, 4 });
+            //HowSumTab(7, new List<int>() { 5, 3, 4 });
+            //BestSumTab(7, new List<int>() { 5, 3, 4 });
+            //AllSumTab(8, new List<int>() { 2, 3, 5 });
+            //AllSumTab(40, new List<int>() { 2, 3, 5 });
+            CountSumTab(40, new List<int>() { 2, 3, 5 });
+
+            //canConstructTab("abcdef", new List<string>() { "ab","abc","cd","def","abcd"});
+            //canConstructTab("skateboard", new List<string>() { "bo","rd","ate","t","ska","sk","boar"});
+            //canConstructTab("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", 
+            //    new List<string>() { "eeeee","eeeeeee","eeeeeeee","eeeeeeeeeee","eeeeeeeeeeee","eeeeeeeeeeeeee","eeeeeeeeeeeeee"});
+            //CountConstructTab("abcdef", new List<string>() { "ab", "abc", "cd", "def", "abcd" });
+            //CountConstructTab("skateboard", new List<string>() { "bo", "rd", "ate", "t", "ska", "sk", "boar" });
+            //CountConstructTab("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+            //    new List<string>() { "eeeee", "eeeeeee", "eeeeeeee", "eeeeeeeeeee", "eeeeeeeeeeee", "eeeeeeeeeeeeee", "eeeeeeeeeeeeee" });
+            //CountConstructTab("enterapotentpot", new List<string>() { "a", "p", "ent", "enter", "ot","o","t" });
+
+            //AllConstructTab("abcdef", new List<string>() { "ab", "abc", "cd", "def", "abcd" });
+            //AllConstructTab("skateboard", new List<string>() { "bo", "rd", "ate", "t", "ska", "sk", "boar" });
+            //AllConstructTab("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+            //    new List<string>() { "eeeee", "eeeeeee", "eeeeeeee", "eeeeeeeeeee", "eeeeeeeeeeee", "eeeeeeeeeeeeee", "eeeeeeeeeeeeee" });
+            //AllConstructTab("enterapotentpot", new List<string>() { "a", "p", "ent", "enter", "ot", "o", "t" });
+            Console.WriteLine();
+            #endregion
+        }
+
+        private static int CountSumTab(int value, List<int> list)
+        {
+            var table = new List<int>();
+            for (int i = 0; i < value + 1; i++)
+            {
+                table.Add(0);
+            }
+
+            table[0] = 1;
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                foreach (var number in list)
+                {
+                    if (i + number < table.Count)
+                    {
+                        table[i + number] += table[i];
+                    }
+                }
+            }
+            return table[table.Count - 1];
+        }
+
+        private static List<List<int>> AllSumTab(int value, List<int> list)
+        {
+            var table = new List<List<List<int>>>();
+
+            for (int i = 0; i < value + 1; i++)
+            {
+                table.Add(new List<List<int>>());
+            }
+
+            table[0].Add(new List<int>());
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                foreach (var number in list)
+                {
+                    var currentIndex = i + number;
+                    if (currentIndex < table.Count)
+                    {
+                        var currentIndexListTable = table[i].DeepClone();
+                        currentIndexListTable.ForEach(x =>
+                        {
+                            x.Add(number);
+                        });
+                        table[currentIndex].AddRange(currentIndexListTable);
+                    }
+                }
+            }
+            return table[table.Count - 1];
+        }
+
+        private static List<List<string>> AllConstructTab(string value, List<string> list)
+        {
+            var table = new List<List<List<string>>>();
+            for (int i = 0; i < value.Length + 1; i++)
+            {
+                table.Add(new List<List<string>>());
+            }
+
+            table[0].Add(new List<string>());
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                foreach (var word in list)
+                {
+                    if (i + word.Length < table.Count)
+                    {
+                        var subString = value.Substring(i, word.Length);
+                        if (subString == word)
+                        {
+                            var currentIndexList = table[i].DeepClone();
+                            currentIndexList.ForEach(x =>
+                            {
+                                x.Add(word);
+                            });
+                            table[i + word.Length].AddRange(currentIndexList);
+                        }
+                    }
+                }
+            }
+            return table[table.Count - 1];
+        }
+
+        private static int CountConstructTab(string value, List<string> list)
+        {
+            var table = new List<int>();
+
+            for (int i = 0; i < value.Length + 1; i++)
+            {
+                table.Add(0);
+            }
+
+            table[0] = 1;
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                foreach (var word in list)
+                {
+                    if (i + word.Length < table.Count)
+                    {
+                        string subString = value.Substring(i, word.Length);
+                        if (subString == word)
+                        {
+                            table[i + word.Length] += table[i];
+                        }
+                    }
+                }
+            }
+
+            return table[table.Count - 1];
+        }
+
+        private static bool canConstructTab(string value, List<string> list)
+        {
+            var table = new List<bool>(value.Length + 1);
+            for (int i = 0; i < value.Length + 1; i++)
+            {
+                table.Add(false);
+            }
+
+            table[0] = true;
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                if (table[i] == true)
+                {
+                    foreach (var word in list)
+                    {
+                        if (i + word.Length < table.Count)
+                        {
+                            var subString = value.Substring(i, word.Length);
+                            if (subString == word)
+                            {
+                                table[i + word.Length] = true;
+                            }
+                        }
+                    }
+                }
+            }
+            return table[table.Count - 1];
+        }
+
+        private static List<int> BestSumTab(int value, List<int> list)
+        {
+            //Initial table
+            var table = new List<List<int>>();
+            for (int i = 0; i < value + 1; i++)
+            {
+                table.Add(null);
+            }
+
+            //Initial Value
+            table[0] = new List<int>();
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                if (table[i] != null)
+                {
+                    foreach (var number in list)
+                    {
+                        var currentIndex = i + number;
+                        //Chống OutOfRange
+                        if (currentIndex < table.Count)
+                        {
+                            if (table[currentIndex] == null)
+                            {
+                                table[currentIndex] = table[i].DeepClone();
+                                table[currentIndex].Add(number);
+                            }
+                            //Tất cả các mảng null sẽ được gán trước khi check
+                            //Kiểm tra bằng nhau thì không gán --> gán ==> Dài hơn
+                            else if (table[currentIndex].Count != table[i].Count)
+                            {
+                                table[currentIndex] = table[i].DeepClone();
+                                table[currentIndex].Add(number);
+                            }
+                        }
+                    }
+                }
+            }
+            return table[table.Count - 1];
+        }
+
+        /// <summary>
+        /// Dựa vào tính chất có thể tái tạo được index cuối cùng
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private static List<int> HowSumTab(int value, List<int> list)
+        {
+            //Initial table 
+            var table = new List<List<int>>();
+            for (int i = 0; i < value + 1; i++)
+            {
+                table.Add(null);
+            }
+            //Initial default value
+            table[0] = new List<int>();
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                if (table[i] != null)
+                {
+                    foreach (var number in list) if (i + number < table.Count)
+                    {
+                        table[i + number] = table[i].DeepClone();
+                        table[i + number].Add(number);
+                    }
+                }
+            }
+            return table[table.Count - 1];
+        }
+
+        /// <summary>
+        /// Dựa vào tính chất của index mảng
+        /// Vì index = 0 ban đầu luôn có thể phân chia hết cho các số 3,4,5 --> True
+        /// Khi xét đến 3,4,5 thì nó có thể cấu tạo được số 6,7,8           --> True
+        /// Lấy kết quả cuối cùng của array 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private static bool CanSumTab(int value, List<int> list)
+        {
+            //Initial cache table 
+            var table = new bool[value + 1];
+            //Initial default value tab
+            for (int i = 0; i < value + 1; i++)
+            {
+                table[i] = false;
+            }
+            //Initial value
+            table[0] = true;
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] == true)
+                {
+                    foreach (var number in list) if (i + number < table.Length)
+                    {
+                        //Xét vị trí có thể cấu tạo thành số 7 hay không ?
+                        table[i + number] = true;
+                    }
+                }
+            }
+            return table[value];
+        }
+        /// <summary>
+        /// Vì nó là 2 tham số chạy giảm dần
+        /// Cần mảng 2 chiều để biểu diễn
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static int GridTraversalTab(int m, int n)
+        {
+            //Tạo matrix có m + 1 hàng, n + 1 cột
+            var matrix = new int[m + 1,n + 1];
+            for (int i = 0; i < m + 1; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    matrix[i,j] = 0;
+                }
+            }
+
+            //Setup initial value
+            matrix[1,1] = 1;
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    var currentVal = matrix[i,j];
+                    //Chặn cột
+                    if (j + 1 <= n)
+                    {
+                        matrix[i, j + 1] += currentVal;
+                    }
+                    //Chặn hàng
+                    if (i + 1 <= m)
+                    {
+                        matrix[i + 1, j] += currentVal;
+                    }
+                }
+            }
+
+            for (int i = 0; i < m + 1; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            return matrix[m, n];
+        }
+        /// <summary>
+        /// Cần xem xét lại công thức Fibonacy 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static int FibTab(int value)
+        {
+            //Xây dựng array full 0 gồm value.lenght phần tử
+            //Để tiến hành cache phần tử phía trước cho current value
+            //Tạo table gồm n + 1 phần tử
+            var table = new List<int>(value + 1);
+            //Seed 0 vào table
+            for (int i = 0; i < value + 1; i++)
+            {
+                table.Add(0);
+            }
+            //Khởi tạo giá trị <==> điều kiện dừng trong Recursion
+            table[1] = 1;
+            for (int i = 0; i <value; i++)
+            {
+                //Tính chất của Fib
+                //Số hiện tại bằng 2 số trước cộng lại
+                table[i+1] += table[i];
+                //Cẩn thận OutOfRange
+                //Khi chưa đến cuối
+                if (i + 2 != value + 1)
+                {
+                    table[i + 2] += table[i];
+                }
+            }
+            return table[value];
         }
 
         private static List<List<string>> AllConstruct(string inputString, List<string> list)
